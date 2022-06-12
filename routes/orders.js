64d4854f397a2e2089f5d4d65e8acec838ebbc1c
RITIK MAKHIJA <35072415+ritik-prog@users.noreply.github.com>
2022-06-12 11:41:30 +0530

    Create orders.js

---
+++ routes/orders.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const {getOrders, getOrder, deleteOrder, placeOrder, updateOrder} = require('../controllers/orders')
+
+const router = express.Router()
+
+router.route('/').get(getOrders).post(placeOrder)
+
+router.route('/:id').get(getOrder).put(updateOrder).delete(deleteOrder)
+
+// router.get('/checkout', checkout)
+
+module.exports = router
